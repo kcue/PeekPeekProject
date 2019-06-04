2d9e157c670ff7836ac1e21381055e247a780378
Tayler Nielsen <tanielse@uci.edu>
2019-06-04 16:33:19 -0700

    Removed JQuery since it wasn't needed

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -34,7 +34,7 @@
                     <input placeholder="Email" type="text" v-model="formData.contact.email"/>
                 </div>
 
-                <div class="phone form-element">e
+                <div class="phone form-element">
                     <input placeholder="Phone" type="text" v-model="formData.contact.phone"/>
                 </div>
 
@@ -50,7 +50,6 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import $ from 'jquery';
 
 @Component
 export default class Form extends Vue {
