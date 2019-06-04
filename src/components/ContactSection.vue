4561df4cd81571ac6e9107de5cc2d6f689b7b085
Samantha Rollins <rollinss@uci.edu>
2019-06-03 18:15:52 -0700

    Buttons Linked
    needs style

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,56 +1,56 @@
 <template>
-    <section class="peek-section" id="contact-section">
-        <div class="contact-section-interlude">
-            <p>So, </p>
-            <p>your turn</p>
-        </div>
-        <div class="contact-section-main">
-            <h1>Tell me</h1>
-            <h1>your story</h1>
-            <button>Contact Us</button>
-        </div>
-    </section>
+  <section class="peek-section" id="contact-section">
+    <div class="contact-section-interlude">
+      <p>So,</p>
+      <p>your turn</p>
+    </div>
+    <div class="contact-section-main">
+      <h1>Tell me</h1>
+      <h1>your story</h1>
+      <button><a href="https://www.peekpeek.com/contact">Contact Us</a></button>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class ContactSection extends Vue {
-    
-}
+export default class ContactSection extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
 #contact-section {
+  justify-content: center;
+
+  .contact-section-interlude {
+    margin-top: 100px;
+
+    p {
+      font-size: 5.5vh;
+      text-align: left;
+    }
+  }
+
+  .contact-section-main {
+    display: flex;
+    flex-direction: column;
     justify-content: center;
 
-    .contact-section-interlude {
-        margin-top: 100px;
-
-        p {
-            font-size: 5.5vh;
-            text-align: left;
-        }
+    h1 {
+      margin-top: 0;
+      margin-bottom: 0;
+      text-align: center;
     }
 
-    .contact-section-main {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-
-        h1 {
-            margin-top: 0;
-            margin-bottom: 0;
-            text-align: center;
-        }
-
-        button {
-            font-size: 4vh;
-            width: 300px;
-            margin: 20px auto 20px auto;
-        }
+    button {
+      font-size: 4vh;
+      width: 300px;
+      margin: 20px auto 20px auto;
     }
+  }
 }
-
 </style>
