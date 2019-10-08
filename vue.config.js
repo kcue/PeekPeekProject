cc76c3b95b2888cb1e1a26eb32d407f0785ce766
kencue <kencue@gmail.com>
2019-10-08 12:00:40 -0700

    update packages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     publicPath: '/',
+    configureWebpack: {
+        devServer: {
+            watchOptions: {
+              ignored: ['node_modules'],
+              aggregateTimeout: 300,
+              poll: 1500
+            },
+            public: '0.0.0.0' // vagrant machine address
+        }
+    },
 };
