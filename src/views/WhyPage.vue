abef5fcb527d0c29d9322e7f7dc79e6bb8fe2d55
sazedas <42198773+sazedas@users.noreply.github.com>
2019-11-09 15:44:38 -0800

    Update WhyPage.vue

--- src/views/WhyPage.vue
+++ src/views/WhyPage.vue
@@ -6,62 +6,62 @@
 		<h3 style="position:absolute; top:205px; left:120px; font-size:30px;color: grey;">Modern Challenges</h3>
 		<h3 style="position:absolute; top:235px; left:120px; font-size:30px;color: #1BB4A6;"> Modern Solution </h3>
 		
-		
-		<img src="../assets/images/demo-fish_on_tap.png" style="position:absolute; top:90px; left:500px; width:700px; height:290px; border-radius: 2%">		
-		<div style= "background-color: #FFF; position:absolute;  top:350px; left:520px; width:700px; height:330px;  border-radius: 6%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"></div>
-		
-    <h3 style="position:absolute; top:370px; left:570px; font-size:9px;color: grey;">CASE STUDY</h3>
+		<div>
+			<img src="../assets/images/demo-fish_on_tap.png" style="position:relative; top:90px; left:500px; width:50%; height:290px; border-radius: 2%">		
+			<div style= "background-color: #FFF; position:absolute;  top:350px; left:520px; width:50%; height:330px;  border-radius: 6%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"></div>
+		</div>
+		<h3 style="position:absolute; top:370px; left:570px; font-size:9px;color: grey;">CASE STUDY</h3>
 		<h3 style="position:absolute; top:375px; left:570px; font-size:22px;color: #1BB4A6;"> Education</h3>
       
     
     
-    <div class="TOP_RIGHT_IMAGE_left_indent">
-      <h3 >Problem</h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit 
-    </div>
+		<div class="top-right-image-left-indent">
+		  <h3 >Problem</h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+		</div>
+			
+		<div class="top-right-image-centered-indent">    
+		  <h3 >Solution</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+		</div>
+			
+		<div class="top-right-image-right-indent">
+		  <h3 >Result</h3>Lorem ipsum dolor sit amet 
+		</div>	
 		
-    <div class="TOP_RIGHT_IMAGE_centered_indent">    
-      <h3 >Solution</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit 
-    </div>
-		
-    <div class="TOP_RIGHT_IMAGE_right_indent">
-      <h3 >Result</h3>Lorem ipsum dolor sit amet 
-    </div>	
+		<div class="top-right-image-button" style="background-color: #1BB4A6;">
+		  <b>New Solution</b>
+		</div>
     
-    <div class="TOP_RIGHT_IMAGE_button" style="background-color: #1BB4A6;">
-      <b>New Solution</b>
-    </div>
-    
-    <img src="../assets/images/demo-uci_arc.png" style="position:absolute; top:650px; left:120px; width:700px; height:290px; border-radius: 2%" >
-		
-		<div style= "background-color: #FFF; position:absolute;  top:920px; left:140px; width:700px; height:330px;  border-radius: 6%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"></div>
-		  <h3 style="position:absolute; top:945px; left:195px; font-size:9px;color: grey;">CASE STUDY</h3>
-		  <h3 style="position:absolute; top:950px; left:195px; font-size:22px;color: #1BB4A6;"> Hotel</h3>
-			<div class="MIDDLE_LEFT_IMAGE_left_indent"><h3 >Problem</h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
-			<div class="MIDDLE_LEFT_IMAGE_centered_indent"><h3 >Solution</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
-			<div class="MIDDLE_LEFT_IMAGE_right_indent"><h3 >Result</h3>Lorem ipsum dolor sit amet </div>			
-			<div class="MIDDLE_LEFT_IMAGE_button" style= "background-color: #1BB4A6;"> <b>New Solution</b></div>
+		<div>
+			<img src="../assets/images/demo-uci_arc.png" style="position:relative; top:350px; left:120px; width:50%; height:290px; border-radius: 2%" >		
+			<div style= "background-color: #FFF; position:absolute;  top:920px; left:140px; width:50%; height:330px;  border-radius: 6%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"></div>
+		</div>
+			  <h3 style="position:absolute; top:945px; left:195px; font-size:9px;color: grey;">CASE STUDY</h3>
+			  <h3 style="position:absolute; top:950px; left:195px; font-size:22px;color: #1BB4A6;"> Hotel</h3>
+				<div class="middle-left-image-left-indent"><h3 >Problem</h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
+				<div class="middle-left-image-centered-indent"><h3 >Solution</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
+				<div class="middle-left-image-right-indent"><h3 >Result</h3>Lorem ipsum dolor sit amet </div>			
+				<div class="middle-left-image-button" style= "background-color: #1BB4A6;"> <b>New Solution</b></div>
 
-	
-		<img src="../assets/images/demo-csulb.png" style="position:absolute; top:1235px; left:500px; width:700px; height:290px; border-radius: 2%" >
 		
-		<div style= "background-color: #FFF; position:absolute;  top:1505px; left:520px; width:700px; height:330px;  border-radius: 6%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"></div>
-		  <h3 style="position:absolute; top:1530px; left:570px; font-size:9px;color: grey;">CASE STUDY</h3>
-		  <h3 style="position:absolute; top:1535px; left:570px; font-size:22px;color: #1BB4A6;"> Brand Solution</h3>
-			<div class="BOTTOM_RIGHT_IMAGE_left_indent"><h3>Problem</h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
-			<div class="BOTTOM_RIGHT_IMAGE_centered_indent"><h3>Solution</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
-			<div class="BOTTOM_RIGHT_IMAGE_right_indent"><h3 >Result</h3>Lorem ipsum dolor sit amet </div>
-				
-			<div class="BOTTOM_RIGHT_IMAGE_button" style= "background-color: #1BB4A6;"> <b>New solution	</b></div>
-    
-     
-  </div>
+		<div>	
+			<img src="../assets/images/demo-csulb.png" style="position:relative; top:635px; left:500px; width:700px; height:290px; border-radius: 2%" >		
+			<div style= "background-color: #FFF; position:absolute;  top:1485px; left:520px; width:700px; height:330px;  border-radius: 6%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"></div>
+		</div>	  
+			  
+			  <h3 style="position:absolute; top:1530px; left:570px; font-size:9px;color: grey;">CASE STUDY</h3>
+			  <h3 style="position:absolute; top:1535px; left:570px; font-size:22px;color: #1BB4A6;"> Brand Solution</h3>
+				<div class="bottom-right-image-left-indent"><h3>Problem</h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
+				<div class="bottom-right-image-centered-indent"><h3>Solution</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit </div>
+				<div class="bottom-right-image-right-indent"><h3 >Result</h3>Lorem ipsum dolor sit amet </div>				
+				<div class="bottom-right-image-button" style= "background-color: #1BB4A6;"> <b>New solution	</b></div>    		 
+	</div>
 </template>
 
 
 
 <style lang="scss" scoped>
 
-.TOP_RIGHT_IMAGE_left_indent {
+.top-right-image-left-indent {
   position: absolute;
   top: 420px;
   left: 570px;
@@ -70,7 +70,7 @@
   font-size:13px; 
   font-family: Open Sans;}
 
-.TOP_RIGHT_IMAGE_right_indent {
+.top-right-image-right-indent {
   position: absolute;
   top: 420px;
   left: 1025px;
@@ -80,7 +80,7 @@
   font-family: Open Sans;}
 
 
-.TOP_RIGHT_IMAGE_centered_indent {
+.top-right-image-centered-indent {
   position: absolute;
   top: 420px;
   left: 810px;
@@ -90,10 +90,10 @@
   font-family: Open Sans;}
 
 
-.TOP_RIGHT_IMAGE_button{
+.top-right-image-button{
   position: absolute;
   top: 540px;
-  left: 970px;
+  left: 950px;
   color: white;
   -webkit-border-radius: 28;
   -moz-border-radius: 28;
@@ -103,7 +103,7 @@
   text-decoration: none;
   font-family: Open Sans;}
 
-.MIDDLE_LEFT_IMAGE_left_indent {
+.middle-left-image-left-indent{
   position: absolute;
   top: 1010px;
   left: 195px;
@@ -112,7 +112,7 @@
   font-size:13px; 
   font-family: Open Sans;}
 
-.MIDDLE_LEFT_IMAGE_right_indent {
+.middle-left-image-right-indent{
   position: absolute;
   top: 1010px;
   left: 645px;
@@ -121,7 +121,7 @@
   font-size:13px; 
   font-family: Open Sans;}
 
-.MIDDLE_LEFT_IMAGE_centered_indent {
+.middle-left-image-centered-indent {
   position: absolute;
   top: 1010px;
   left: 430px;
@@ -130,10 +130,10 @@
   font-size:13px; 
   font-family: Open Sans;}
   
-.MIDDLE_LEFT_IMAGE_button {
+.middle-left-image-button{
   position: absolute;
   top: 1115px;
-  left: 600px;
+  left: 570px;
   color: white;
   -webkit-border-radius: 28;
   -moz-border-radius: 28;
@@ -145,7 +145,7 @@
 
 
 
-.BOTTOM_RIGHT_IMAGE_left_indent {
+.bottom-right-image-left-indent{
   position: absolute;
   top: 1585px;
   left: 570px;
@@ -154,7 +154,7 @@
   font-size:13px; 
   font-family: Open Sans;}
 
-.BOTTOM_RIGHT_IMAGE_right_indent {
+.bottom-right-image-right-indent {
   position: absolute;
   top: 1585px;
   left: 1025px;
@@ -164,7 +164,7 @@
   font-family: Open Sans;}
 
 
-.BOTTOM_RIGHT_IMAGE_centered_indent {
+.bottom-right-image-centered-indent {
   position: absolute;
   top: 1585px;
   left: 810px;
@@ -174,7 +174,7 @@
   font-family: Open Sans;}
 
 
-.BOTTOM_RIGHT_IMAGE_button {
+.bottom-right-image-button{
   position: absolute;
   top: 1700px;
   left: 970px;
